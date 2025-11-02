700b79ff579501731ca3f97ba25be92b83c518b3
Harsh <hbamaniyachoudhary@gmail.com>
2025-11-02 19:18:42 +0530

    Igniting App E-2

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+console.log("✅ App.js loaded");
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an H1 Tag!"),
-    React.createElement("h2", {}, "I'm an H2 Tag!"),
+  React.createElement("div", { id: "child", key: "child-1" }, [
+    React.createElement("h1", { key: "c1-h1" }, "I love coding🚀"),
+    React.createElement("h2", { key: "c1-h2" }, "Lovely !"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an H1 Tag!"),
-    React.createElement("h2", {}, "I'm an H2 Tag!"),
+  React.createElement("div", { id: "child2", key: "child-2" }, [
+    React.createElement("h1", { key: "c2-h1" }, "Arey Bhai!!"),
+    React.createElement("h2", { key: "c2-h2" }, "I  in the D folder"),
   ]),
 ]);
-
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
