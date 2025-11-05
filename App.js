e9d155ba756c4f821f7664de636ca1786397559a
Harsh <hbamaniyachoudhary@gmail.com>
2025-11-05 21:54:11 +0530

    E-3

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-console.log("✅ App.js loaded");
+const Title = () => (
+  <h1 id="title" className="title">
+    Harsh Bamaniya
+  </h1>
+);
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "child-1" }, [
-    React.createElement("h1", { key: "c1-h1" }, "I love coding🚀"),
-    React.createElement("h2", { key: "c1-h2" }, "Lovely !"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child-2" }, [
-    React.createElement("h1", { key: "c2-h1" }, "Arey Bhai!!"),
-    React.createElement("h2", { key: "c2-h2" }, "I  in the D folder"),
-  ]),
-]);
-
+const HeadingComponent = () => {
+  return (
+    <div>
+      {Title()}
+      <Title></Title>
+      <h1>React Functional Component</h1>
+      <Title />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
